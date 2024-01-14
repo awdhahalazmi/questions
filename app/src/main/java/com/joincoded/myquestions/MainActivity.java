060f65c70f58e83b112a10f49e8b2642b3fb9f78
Awdhah A N H S A Alazemi <83318@kfh.com>
2024-01-14 15:28:08 +0300

    android task

---
+++ app/src/main/java/com/joincoded/myquestions/MainActivity.java
@@ -0,0 +1,115 @@
+package com.joincoded.myquestions;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView questionTextView, correctTextView, wrongTextView, GradeTextView;
+    private Button trueButton, falseButton, nextButton;
+
+    private ArrayList<String> questions;
+    private ArrayList<Boolean> answers;
+    private int currentIndex = 0;
+
+    int score = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        questionTextView = findViewById(R.id.questionTextView);
+        correctTextView = findViewById(R.id.correctTextView);
+        wrongTextView = findViewById(R.id.WrongTextView);
+        trueButton = findViewById(R.id.truee);
+        falseButton = findViewById(R.id.falsee);
+        nextButton = findViewById(R.id.nextQ);
+        GradeTextView = findViewById(R.id.GradeTextView);
+
+
+        questions = new ArrayList<>();
+        answers = new ArrayList<>();
+
+
+        questions.add("penguins can fly");
+        answers.add(false);
+
+        questions.add("the water boils at 200c");
+        answers.add(false);
+
+        questions.add("people can fly");
+        answers.add(true);
+
+        displayQuestion();
+
+        trueButton.setOnClickListener(view -> checkAnswer(true));
+        falseButton.setOnClickListener(view -> checkAnswer(false));
+        nextButton.setOnClickListener(view -> nextQuestion());
+    }
+
+    private void displayQuestion() {
+        questionTextView.setText(questions.get(currentIndex));
+
+        GradeTextView.setText(String.valueOf(score));
+
+        correctTextView.setVisibility(View.INVISIBLE);
+        wrongTextView.setVisibility(View.INVISIBLE);
+        nextButton.setVisibility(View.INVISIBLE);
+        trueButton.setVisibility(View.VISIBLE);
+        falseButton.setVisibility(View.VISIBLE);
+    }
+
+    private void checkAnswer(boolean userAnswer) {
+        boolean correctAnswer = answers.get(currentIndex);
+
+        if (userAnswer == correctAnswer) {
+
+            correctTextView.setVisibility(View.VISIBLE);
+            wrongTextView.setVisibility(View.INVISIBLE);
+            trueButton.setVisibility(View.INVISIBLE);
+            falseButton.setVisibility(View.INVISIBLE);
+            nextButton.setVisibility(View.VISIBLE);
+
+            TheGrade();
+        } else {
+
+            correctTextView.setVisibility(View.INVISIBLE);
+            wrongTextView.setVisibility(View.VISIBLE);
+            trueButton.setVisibility(View.VISIBLE);
+            falseButton.setVisibility(View.VISIBLE);
+            nextButton.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void TheGrade() {
+        score++;
+        GradeTextView.setText(String.valueOf(score));
+    }
+
+    private void nextQuestion() {
+        currentIndex++;
+        if (currentIndex < questions.size()) {
+            displayQuestion();
+        } else {
+
+        }
+    }
+}
+
+
+//public class MainActivity extends AppCompatActivity {
+
+//@Override
+//protected void onCreate(Bundle savedInstanceState) {
+//  super.onCreate(savedInstanceState);
+//setContentView(R.layout.activity_main);
+//}
+//}
